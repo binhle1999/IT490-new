9aa106300bd102fd0b179ce20e9f967a82adc42d
binhle1999 <55223590+binhle1999@users.noreply.github.com>
2022-05-09 12:35:14 -0400

    Update loginpage.php

--- frontend (copy)/loginpage.php
+++ frontend (copy)/loginpage.php
@@ -49,35 +49,23 @@ if($host == "off")
 	echo $node .PHP_EOL;
 }
 $client = new rabbitMQClient($node,"testServer");
-$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
-$password = $_POST["password"];
-$hashed_pass = password_hash($password, PASSWORD_DEFAULT);
 $request = array();
 $request['type'] = "login";
 $request['username'] = $_POST["username"];
-$request['password'] = $password;
-$request['hash'] = $hashed_pass;
+$request['password'] = $_POST["password"];;
 $response = $client->send_request($request);
-//$errFlag = False;
-//$nameErr = False;
-//$passErr = False;
 
 if($response == 1){
-	//$error = date("Y-m-d") . "  " . date("h:i:sa") . "  --- Frontend --- " . "Error: failed to login using Username = " . $_POST["uname"] . " and Password = " . $_POST["psw"] . "\n";
-	//log_event($error);
 	//response received, user authorized
 	$_SESSION["username"] = $_POST["username"];
         header("Location: home.php");
 	
 	
 } else{
-	//$event = date("Y-m-d") . "  " . date("h:i:sa") . "Login successful using Username = " . $_POST["username"] . " and Password = " . $_POST["password"] . "\n";
-	//log_event($event);
-	
 	//user not found
         header("Location: loginpage.php");
 	$msg = "Unauthorized.\nTry Again";
-        echo "<script type='text/javascript'>alert('$msg');</script>";
+        echo "<script type='text/javascript'>alert('Unauthorized.\nTry Again');</script>";
 
 }
 
